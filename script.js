81b236be73877ecd5517a5bf3bdbeb70a34386c4
VinoMurugan <137902991+VinoMurugan@users.noreply.github.com>
2023-08-17 18:05:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+function showQuestion(question, correctAnswer) {
+    const playerAnswer = prompt(`${question}\n\nWhat's your answer?`);
+    if (playerAnswer !== null) {
+        if (playerAnswer.trim().toLowerCase() === correctAnswer.toLowerCase()) {
+            alert(`Correct! You answered correctly.`);
+        } else {
+            alert(`Incorrect! The correct answer is: ${correctAnswer}`);
+        }
+    }
+}
