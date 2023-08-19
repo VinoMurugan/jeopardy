1fae78d93b5f75781ac81051d86dbc206b972ea3
VinoMurugan <137902991+VinoMurugan@users.noreply.github.com>
2023-08-19 07:35:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-function showQuestion(question, correctAnswer) {
+let playerScore = 0;
+  
+function showQuestion(question, correctAnswer,amount) {
     const playerAnswer = prompt(`${question}\n\nWhat's your answer?`);
     if (playerAnswer !== null) {
-        if (playerAnswer.trim().toLowerCase() === correctAnswer.toLowerCase()) {
-            alert(`Correct! You answered correctly.`);
+        const showPlayerAnswer = playerAnswer.trim().toLowerCase();
+        const showCorrectAnswer = correctAnswer.split(":")[1].trim().toLowerCase();
+        
+        if (showPlayerAnswer === showCorrectAnswer) {
+            alert(`Correct! WOW your answer is Right.`);
+            playerScore += parseInt(amount.slice(1)); 
+            document.getElementById("player-score").textContent = `$${playerScore}`;
         } else {
-            alert(`Incorrect! The correct answer is: ${correctAnswer}`);
+            alert(`Incorrect! oops no correct answer is : ${correctAnswer}`);
+            playerScore -= parseInt(amount.slice(1)); 
+            document.getElementById("player-score").textContent = `$${playerScore}`;      
         }
     }
 }
+
+
+
+
